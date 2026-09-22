576afa3da3b80fc90f1586ae210b6084934c503a
DIE <departamentoinnovacioneducativa@juventud.edu.mx>
2026-09-22 14:13:10 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'org-extra-v1.0'; 
+const CACHE_NAME = 'org-extra-v2.0'; 
 const urlsToCache = ['./', './index.html', './styles.css', './script.js', './manifest.json'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(cacheNames => { return Promise.all(cacheNames.map(cacheName => { if (cacheName !== CACHE_NAME) return caches.delete(cacheName); })); }).then(() => self.clients.claim())); });
